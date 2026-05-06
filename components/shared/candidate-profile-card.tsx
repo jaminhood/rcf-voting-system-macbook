c649f236ad41cc19f9ae9f6ad9627a8dcd394de0
jaminhood <princeobj5@gmail.com>
2026-05-06 13:44:41 +0100

    style: fix light theme card style

--- components/shared/candidate-profile-card.tsx
+++ components/shared/candidate-profile-card.tsx
@@ -19,19 +19,19 @@ const CandidateProfileCard: FC<{ candidate: Candidate }> = ({ candidate }) => {
 					/>
 				</div>
 				<div className="p-4 -mt-8 rounded-t-2xl dark:bg-zinc-100 bg-zinc-400 z-10 transition-all ease-in-out duration-300">
-					<h2 className="dark:text-zinc-950 text-zinc-100 font-black text-2xl h-20">{truncate(candidate.name, 26)}</h2>
-					<h6 className="dark:text-zinc-800 text-zinc-200 italic text-xs">
+					<h2 className="text-zinc-950 font-black text-2xl h-20">{truncate(candidate.name, 26)}</h2>
+					<h6 className="text-zinc-800 italic text-xs">
 						{"{"} {candidate.position} {"}"}
 					</h6>
 					<hr className="my-2 border-t border-zinc-300" />
 					<div className="flex justify-between items-center">
-						<h5 className="dark:text-zinc-950 text-zinc-100 font-black text-sm">Votes Count:</h5>
-						<h5 className="dark:text-zinc-950 text-zinc-200 font-black text-sm">{candidate.votes}</h5>
+						<h5 className="text-zinc-950 font-black text-sm">Votes Count:</h5>
+						<h5 className="text-zinc-950 font-black text-sm">{candidate.votes}</h5>
 					</div>
 					<div>
 						<button
 							onClick={() => setShowModal(true)}
-							className="h-12 w-full cursor-pointer text-zinc-400 inline-flex items-center mt-4 justify-center rounded-2xl uppercase font-black tracking-widest border border-solid border-black/8 px-5 transition-colors dark:bg-zinc-950 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-full">
+							className="h-12 w-full cursor-pointer bg-zinc-950 text-zinc-100 inline-flex items-center mt-4 justify-center rounded-2xl uppercase font-black tracking-widest border border-solid border-black/8 px-5 transition-colors dark:bg-zinc-950 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-full">
 							Vote
 						</button>
 					</div>
