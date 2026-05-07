ea039b805df5c9c9ea9490e3dfae53b87bdfb604
jaminhood <princeobj5@gmail.com>
2026-05-07 13:10:52 +0100

    fix: setting up account info

--- components/frontend/candidate-profile.tsx
+++ components/frontend/candidate-profile.tsx
@@ -27,6 +27,11 @@ const CandidateProfile = () => {
 
 	return (
 		<section className="grid md:grid-cols-3 grid-cols-1 gap-8 w-full p-4 sm:p-6 lg:p-8 max-w-7xl bg-zinc-50 dark:bg-black">
+			<div className="md:col-span-3 justify-center items-center py-4 rounded-xl dark:bg-zinc-100 bg-white">
+				<p className="dark:text-zinc-900 text-zinc-100 text-center text-sm">
+					<strong className="font-black">Note:</strong> To vote please upload your proof of payment. Your request will be reviewed by an admin before your vote is counted.
+				</p>
+			</div>
 			{loading && (
 				<div className="md:col-span-3 justify-center items-center py-8 px-4">
 					<p className="dark:text-zinc-100 text-zinc-900 text-center">Loading Candidates</p>
